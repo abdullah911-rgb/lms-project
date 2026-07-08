fccff78ac1d9b6a0b209204a470fe90917d2d208
abdullah911-rgb <abdullahwasif981@gmail.com>
2026-07-08 12:46:25 +0500

    error resolve

--- client/src/pages/instructor/CourseForm.jsx
+++ client/src/pages/instructor/CourseForm.jsx
@@ -971,7 +971,6 @@ const CourseForm = () => {
                   ></textarea>
                 </div>
 
-                <div className="grid grid-cols-2 gap-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="space-y-1.5">
                     <label className="text-xs font-bold text-slate-505 uppercase tracking-wider block">Scheduled Start Time (Optional)</label>
